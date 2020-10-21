d53d3580079746c42a5f08a8b537f66c1178ee8c
TiagoCicieri <tiagocicei@gmail.com>
2020-10-21 16:53:15 -0300

    Finalizado o 4 dia

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -5,12 +5,20 @@ import { createStackNavigator} from '@react-navigation/stack';
 const { Navigator, Screen } = createStackNavigator();
 
 import OrphanageMap from './pages/OrphanagesMap';
+import OrphanageDetails from './pages/OrphanageDetails';
 
 export default function Routes() {
     return(
         <NavigationContainer>
-            <Navigator>
-                <Screen name="OrphanageMap" component={OrphanageMap} />                
+            <Navigator screenOptions={{ headerShown: false }}>
+                <Screen 
+                    name="OrphanageMap" 
+                    component={OrphanageMap} 
+                />                
+                <Screen 
+                    name="OrphanageDetails" 
+                    component={OrphanageDetails} 
+                />                
             </Navigator>
         </NavigationContainer>
     )
